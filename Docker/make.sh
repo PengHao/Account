87ecc956d8178e6b26f3a2c1523ae68b85c70d3c
彭浩 <wolfpeng.ph@alibaba-inc.com>
2017-11-04 15:17:16 +0800

    change build shell

--- Docker/make.sh
+++ Docker/make.sh
@@ -5,3 +5,5 @@ timestamp=`date +%s`
 docker build -t wolfpeng/$1:1.0.0 ${basepath}
 docker tag wolfpeng/$1:1.0.0 ${REGISTRY}/wolfpeng/$1:latest
 docker push ${REGISTRY}/wolfpeng/$1:latest
+
+ssh wolfpeng@project.wolfpeng.com sh /home/wolfpeng/start.sh
