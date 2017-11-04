b85484c6f538198c3738c32b1ae299652d2de37b
彭浩 <wolfpeng.ph@alibaba-inc.com>
2017-11-04 14:15:06 +0800

    change build shell

--- Docker/make.sh
+++ Docker/make.sh
@@ -1,8 +1,7 @@
 basepath=$(cd `dirname $0`; pwd)
 REGISTRY=registry.wolfpeng.com:5000
-echo $1
 timestamp=`date +%s`
-echo $timestamp
-docker build -t wolfpeng/$1:${timestamp} ${basepath}
-docker tag wolfpeng/$1:${timestamp} ${REGISTRY}/wolfpeng/$1:latest
+
+docker build -t wolfpeng/$1:1.0.0 ${basepath}
+docker tag wolfpeng/$1:1.0.0 ${REGISTRY}/wolfpeng/$1:latest
 docker push ${REGISTRY}/wolfpeng/$1:latest
